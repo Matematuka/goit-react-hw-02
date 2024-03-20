f693dc53c44ea02b822f6a4de9c54784d16aefa6
Matematuka <143654489+Matematuka@users.noreply.github.com>
2024-03-20 21:07:46 +0200

    css

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,14 +1,16 @@
+import css from "./Feedback.module.css"
+
 const Feedback = ({ feedback, totalFeedback, positiveFeedback }) => {
     
   return (
-<div>
+<div className={css.feedbackBox}>
     {totalFeedback > 0 &&
         <ul>
             <li>Good: {feedback.good} </li>
             <li>Neutral: {feedback.neutral}</li>
             <li>Bad: {feedback.bad}</li>
-            <li>Total: {totalFeedback}</li>
-            <li>Positive: {positiveFeedback}%</li>
+            <li className={css.total}>Total: {totalFeedback}</li>
+            <li className={css.positive}>Positive: {positiveFeedback}%</li>
     </ul>}
 </div>
 )
